c70f70ebd7464d7dce2427eed98126deaafdb42b
Jay Quach <kod88vn@gmail.com>
2016-05-20 18:17:43 -0400

    refactoring

--- client/app/components/weather/weather.js
+++ client/app/components/weather/weather.js
@@ -1,10 +1,11 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import weatherComponent from './weather.component';
-import forecastComponent from './forecast/forecast.component';
+import Forecast from './forecast/forecast';
 
 let weatherModule = angular.module('weather', [
-  uiRouter
+  uiRouter,
+  Forecast.name
 ])
 
 .config(($stateProvider) => {
@@ -24,7 +25,6 @@ let weatherModule = angular.module('weather', [
     });
 })
 
-.component('weather', weatherComponent)
-.component('forecast', forecastComponent);
+.component('weather', weatherComponent);
 
 export default weatherModule;
